5046ca55988edc3f2250929e545f02596d2269b7
Sean Story <sean.j.story@gmail.com>
2023-03-22 09:19:46 -0500

    Mimic aon-react-demo-ui

---
+++ src/components/CustomResultView.tsx
@@ -0,0 +1,55 @@
+import {SearchResult} from "@elastic/search-ui";
+
+export const CustomResultView = ({
+                              result,
+                              onClickLink
+                          }: {
+    result: SearchResult;
+    onClickLink: () => void;
+}) => (
+    <li className="sui-result">
+        <div className="sui-result__header">
+            <h3>
+                {/* Maintain onClickLink to correct track click throughs for analytics*/}
+                <a onClick={onClickLink} href={result.url?.raw} dangerouslySetInnerHTML={{__html: result.title?.snippet}}/>
+            </h3>
+            {
+                result.thumbnail_url?.raw &&
+                <span className="sui-result__image">
+                    <img src={result.thumbnail_url?.raw} alt="" style={{height: "100px"}} />
+                </span>
+            }
+        </div>
+        <div className="sui-result__body">
+            {/* use 'raw' values of fields to access values without snippets */}
+
+            {/* Use the 'snippet' property of fields with dangerouslySetInnerHtml to render snippets */}
+            <ul className="sui-result__details">
+                <li>
+                    <span className="sui-result__key">category</span>
+                    <span className="sui-result__value">{result.category?.raw}</span>
+                </li>
+                <li>
+                    <span className="sui-result__key">sub-category</span>
+                    <span className="sui-result__value">{result.sub_category?.raw}</span>
+                </li>
+                <li>
+                    <span className="sui-result__key">short description</span>
+                    <span className="sui-result__value" dangerouslySetInnerHTML={{__html: result.meta_description?.snippet}}/>
+                </li>
+                <li>
+                    <span className="sui-result__key">main content</span>
+                    <span className="sui-result__value" dangerouslySetInnerHTML={{__html: result.body_content?.snippet}}/>
+                </li>
+                {
+                    result.meta_keywords &&
+                    <li>
+                        <span className="sui-result__key">keywords</span>
+                        <span className="sui-result__value">{Object.values(result.meta_keywords?.raw).join(", ")}</span>
+                    </li>
+                }
+            </ul>
+        </div>
+    </li>
+);
+export {}
